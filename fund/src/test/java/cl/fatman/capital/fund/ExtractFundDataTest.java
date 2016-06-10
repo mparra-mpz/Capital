02f3a08f6f2f1bd3e29d81b5ca75e404d0c66487
mparra-mpz <mparra.mpz@gmail.com>
2016-06-10 09:41:20 -0300

    Adapt the unit test to the changes the fund extract data class, add more test for the border conditions.

--- fund/src/test/java/cl/fatman/capital/fund/ExtractFundDataTest.java
+++ fund/src/test/java/cl/fatman/capital/fund/ExtractFundDataTest.java
@@ -1,24 +1,52 @@
 package cl.fatman.capital.fund;
 
 import static org.junit.Assert.*;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class ExtractFundDataTest {
+import java.time.LocalDate;
+import java.util.Map;
+
+public class ExtractFundDataTest {	
 	
-	private static ExtractFundData efData;
 	
-	
-	@BeforeClass
-	public static void setUp() {
+	@Test
+	public void fundDataNotNullTest() {
 		String link = "http://www.aafm.cl/tecnoera/index.php?clase=informe&metodo=rentabilidad_html&inversion=%&administradora=%&tipo=";
 		String userAgent = "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36";
-		efData = new ExtractFundData(link, userAgent);
+		LocalDate queryDate = LocalDate.of(2016, 1, 1);
+		ExtractFundData efData = new ExtractFundData(link, userAgent);
+		assertNotNull("Fund map should not be null", efData.getFundData(queryDate, "Deuda < 90 días", 1));
 	}
 	
 	
 	@Test
-	public void getFundDataTest() {
-		assertNotNull("List with Fund data should not be null", efData.getFundData("Deuda < 90 días", 1));
+	public void fundDataNotEmptyTest() {
+		String link = "http://www.aafm.cl/tecnoera/index.php?clase=informe&metodo=rentabilidad_html&inversion=%&administradora=%&tipo=";
+		String userAgent = "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36";
+		LocalDate queryDate = LocalDate.of(2016, 1, 1);
+		ExtractFundData efData = new ExtractFundData(link, userAgent);
+		Map<Fund, Double>  tmpMap = efData.getFundData(queryDate, "Deuda < 90 días", 1);
+		assertFalse("Fund map should not be empty.", tmpMap.isEmpty()); 
+	}
+	
+	
+	@Test
+	public void fundDataNullTest() {
+		String link = "";
+		String userAgent = "";
+		LocalDate queryDate = LocalDate.of(2016, 1, 1);
+		ExtractFundData efData = new ExtractFundData(link, userAgent);
+		assertNull("Fund map should be null.", efData.getFundData(queryDate, "Deuda < 90 días", 1));
+	}
+	
+	
+	@Test
+	public void fundDataEmptyTest() {
+		String link = "http://www.aafm.cl/tecnoera/index.php?clase=informe&metodo=rentabilidad_html&inversion=%&administradora=%&tipo=";
+		String userAgent = "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36";
+		LocalDate queryDate = LocalDate.of(1970, 1, 1);
+		ExtractFundData efData = new ExtractFundData(link, userAgent);
+		Map<Fund, Double>  tmpMap = efData.getFundData(queryDate, "Deuda < 90 días", 1);
+		assertTrue("Fund map should be empty.", tmpMap.isEmpty()); 
 	}
 }
