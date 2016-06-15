3fe7a09937b1e0867ed4fbb01e90ff14a6dfca01
mparra-mpz <mparra.mpz@gmail.com>
2016-06-15 14:33:35 -0300

    Update the junit test method and add more tests to junit.

--- fund/src/test/java/cl/fatman/capital/fund/PersistenceDataTest.java
+++ fund/src/test/java/cl/fatman/capital/fund/PersistenceDataTest.java
@@ -25,6 +25,12 @@ public class PersistenceDataTest {
 	}
 	
 	@Test
+	public void selectEmptyFomentUnitTest() {
+		List<?> tmpList = connection.selectAllObjects("from FomentUnit", FomentUnit.class);
+		assertEquals("Foment Unit lists should be empty.", 0, tmpList.size());
+	}
+	
+	@Test
 	public void createFomentUnitTest() {
 		LocalDate fuDate = LocalDate.now();
 		List<FomentUnit> fuList = new ArrayList<FomentUnit>();
@@ -35,18 +41,30 @@ public class PersistenceDataTest {
 	}
 	
 	@Test
-	public void createFundEntryTest() {
+	public void selectEmptyFundTest() {
+		List<?> tmpList = connection.selectAllObjects("from Fund", Fund.class);
+		assertEquals("Fund lists should be empty.", 0, tmpList.size());
+	}
+	
+	@Test
+	public void createFundTest() {
 		List<Fund> fundList = new ArrayList<Fund>();
 		fundList.add(new Fund("Fake", "0-K", "A", "REM",  "Accionario"));
 		fundList.add(new Fund("Fake", "0-K", "B", "REM",  "Accionario"));
 		fundList.add(new Fund("Fake", "0-K", "C", "REM",  "Accionario"));
 		connection.insertObjectList(fundList);
 		List<?> tmpList = connection.selectAllObjects("from Fund", Fund.class);
-		assertEquals("Fund lists should be equals", fundList.size(), tmpList.size());
+		assertEquals("Fund lists should be equals.", fundList.size(), tmpList.size());
 	}
 	
 	@Test
-	public void createFundRateEntryTest() {
+	public void selectEmptyFundRateTest() {
+		List<?> tmpList = connection.selectAllObjects("from FundRate", FundRate.class);
+		assertEquals("Fund Rate lists should be equals", 0, tmpList.size());
+	}
+	
+	@Test
+	public void createFundRateTest() {
 		List<FundRate> frList = new ArrayList<FundRate>();
 		List<Fund> fundList = new ArrayList<Fund>();
 		LocalDate frDate = LocalDate.now();
