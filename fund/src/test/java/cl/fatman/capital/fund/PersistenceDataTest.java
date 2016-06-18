95b2bbd57da3de18b03b387a23cac2b4252473b2
mparra-mpz <mparra.mpz@gmail.com>
2016-06-18 02:46:23 -0300

    Add a junit test for too many connections failure.

--- fund/src/test/java/cl/fatman/capital/fund/PersistenceDataTest.java
+++ fund/src/test/java/cl/fatman/capital/fund/PersistenceDataTest.java
@@ -80,7 +80,19 @@ public class PersistenceDataTest {
 	public void selectFundTest() {
 		persistence.insertObjectList(ftList);
 		persistence.insertObjectList(fList);
-		Fund rFund = persistence.selectFund("0-k", "s-26");
+		Fund rFund = persistence.selectFund("0-k", "s-6");
 		assertNotNull("The fund received should not be null", rFund);
+	}
+	
+	@Test
+	public void tooManyConnectionsTest() {
+		persistence.insertObjectList(ftList);
+		persistence.insertObjectList(fList);
+		persistence.insertObjectList(frList);
+		
+		for (int i = 1; i <= 500; i++) {
+			persistence.selectFund("0-k", "s-" + i);
+		}
+		fail("Not yet implemented");
 	}
 }
