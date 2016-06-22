34c6947f3b887bc448fd9e373f3bd9ed955443ee
mparra-mpz <mparra.mpz@gmail.com>
2016-06-21 23:50:32 -0300

    Add the application that load fund data in the database.

--- fund/src/main/java/cl/fatman/capital/fund/App.java
+++ fund/src/main/java/cl/fatman/capital/fund/App.java
@@ -1,13 +1,18 @@
 package cl.fatman.capital.fund;
 
-/**
- * Hello world!
- *
- */
+import java.time.LocalDate;
+
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {	
+    	Controller control = Controller.getInstance();
+    	control.setUp();
+        System.out.println( "Starting application that load the funds data in the database.");
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(4);
+        control.storeFundData(startDate, endDate);
+        control.tearDown();
+        System.out.println( "Finishing application that load the funds data in the database.");
     }
 }
