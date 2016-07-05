ac634e8842e82b94a99d5c73502afa36bae556b7
mparra-mpz <mparra.mpz@gmail.com>
2016-07-05 04:16:17 -0300

    Add junit test for foment unit retrieve fail.

--- fund/src/test/java/cl/fatman/capital/fund/ControllerFailTest.java
+++ fund/src/test/java/cl/fatman/capital/fund/ControllerFailTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ControllerFailTest {
@@ -30,5 +31,14 @@ public class ControllerFailTest {
 		control.storeFundData();
 		List<?> fundRateList = persistence.selectAllObjects("from FundRate", FundRate.class);
 		assertEquals("Fund rate list size shold be 0.", 0, fundRateList.size());
+	}
+	
+	@Test
+	public void storeFomentUnitDataFailTest() {
+		LocalDate startDate = LocalDate.of(1910, 1, 1);
+		LocalDate endDate = LocalDate.of(1912, 1, 1);
+		control.storeFomentUnitData(startDate, endDate);
+		List<?> ufList = persistence.selectAllObjects("from FomentUnit", FomentUnit.class);
+		assertEquals("Foment unit list size shold be 0.", 0, ufList.size());
 	}
 }
