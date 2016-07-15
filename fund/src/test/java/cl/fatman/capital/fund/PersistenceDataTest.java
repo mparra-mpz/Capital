72057cb3e1b550d72b6dcc1d46329d422d841e70
mparra-mpz <mparra.mpz@gmail.com>
2016-07-15 06:05:56 -0300

    Cosmetic change.

--- fund/src/test/java/cl/fatman/capital/fund/PersistenceDataTest.java
+++ fund/src/test/java/cl/fatman/capital/fund/PersistenceDataTest.java
@@ -9,8 +9,8 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PersistenceDataTest {
 	
