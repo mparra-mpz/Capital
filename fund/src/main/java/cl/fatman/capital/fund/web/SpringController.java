179218c458f231bf66abaeb0f8523159267e95ae
mparra-mpz <mparra.mpz@gmail.com>
2016-07-30 12:49:52 -0300

    Move the Controller setup and tear down to spring boot events.

--- fund/src/main/java/cl/fatman/capital/fund/web/SpringController.java
+++ fund/src/main/java/cl/fatman/capital/fund/web/SpringController.java
@@ -1,18 +1,23 @@
 package cl.fatman.capital.fund.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
+import cl.fatman.capital.fund.logic.Controller;
+
 @RestController
+@RequestMapping("/fund")
 public class SpringController {
 	
-	@RequestMapping("/update_date")
-	public String updateDate() {
-		LocalDate updateDate = LocalDate.now();
-		return updateDate.toString();
+	private Controller control = Controller.getInstance();
+	
+	@RequestMapping(value="/update_date", method=RequestMethod.GET)
+	public LocalDate updateDate() {
+		LocalDate updateDate = control.getFundRateUpdateDate();
+		return updateDate;
 	}
 
 }
