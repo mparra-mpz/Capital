f0c2b2d42fb69dd56cfdef9e735849ccf59c4544
mparra-mpz <mparra.mpz@gmail.com>
2016-08-04 20:11:30 -0300

    Fix path for the index.

--- fund/src/main/java/cl/fatman/capital/fund/web/SpringWebController.java
+++ fund/src/main/java/cl/fatman/capital/fund/web/SpringWebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpringWebController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/fund")
     public String index(){
         return "index";
     }
