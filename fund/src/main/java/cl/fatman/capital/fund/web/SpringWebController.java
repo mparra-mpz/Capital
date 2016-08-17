4e5af1c66496ddd10af8b0f7e646b98e76eb3422
mparra-mpz <mparra.mpz@gmail.com>
2016-08-17 08:23:53 -0300

    Add update database for the web controller.

--- fund/src/main/java/cl/fatman/capital/fund/web/SpringWebController.java
+++ fund/src/main/java/cl/fatman/capital/fund/web/SpringWebController.java
@@ -2,13 +2,20 @@ package cl.fatman.capital.fund.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/fund")
 public class SpringWebController {
 	
-	@RequestMapping("/fund")
+	@RequestMapping(value="", method=RequestMethod.GET)
     public String index(){
         return "index";
+    }
+	
+	@RequestMapping(value="/update", method=RequestMethod.GET)
+    public String update(){
+        return "update";
     }
 
 }
